88bb603193f3a0c577c9c3d4d593103a1e1f65a4
Jimmy Walker <79887392+Jwalker5577@users.noreply.github.com>
2022-10-22 20:12:58 -0400

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    title: "Quote-Generator App",
+    subtitle: "Inspirational Quote-Generator",
+    description:
+      "A quote generator that uses HTML, CSS, and Javascript.  This app displays inspirational quotes from an API.  You can click a button for a new one and another button to tweet it out.",
+    image: 'Quoteee.png',
+    link: "https://walkerjj-quotegenerator.netlify.app",
+  },
+  {
+    title: "Crypto Tracker App",
+    subtitle: "Cryptocurrency Tracker",
+    description:
+      "This is a crypto tracker built with React.  Uses an API with the use state and use effect hook.  Able to view changes in your cryptocurreny and search for them in the search bar.    ",
+    image: "Cryptoo.png",
+    link: "https://walkerjj-crypto.netlify.app",
+  },
+  {
+    title: "Pup Finder",
+    subtitle: "Dogs For Sale",
+    description:
+      "App built using React.  Able to view and post listings.  Able to create an account and login with either Email or Google Auth.  App connected to a Firebase database to store listings.  ",
+    image: "pupfinder.PNG",
+    link: "https://walkerjj-pup-finder.netlify.app/",
+  },
+  {
+    title: "Book Tracker",
+    subtitle: "Full Stack Book Tracker",
+    description:
+      "App built with HTML, CSS, and Javascript.  Connected to a MongoDB database and deployed with Heroku.  Able to log and keep track of your books using the database.  ",
+    image: "Lukasbook.PNG",
+    link: "https://books-jdub.herokuapp.com/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Bootstrap",
+  "Node.js",
+  "React",
+  "Wordpress",
+  "MongoDB",
+  "Firebase",
+  "REST",
+  "GitHub"
+];
